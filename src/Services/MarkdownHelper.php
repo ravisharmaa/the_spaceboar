093a55f57b5e1953263d85cff6fb2168407e864f
ravi <ravi.sharma@javra.com>
2019-07-09 12:51:54 +0545

    Config and Parameters in Synfony

--- src/Services/MarkdownHelper.php
+++ src/Services/MarkdownHelper.php
@@ -23,19 +23,25 @@ class MarkdownHelper
      * @var LoggerInterface
      */
     protected $logger;
+    /**
+     * @var bool
+     */
+    private $isDebug;
 
     /**
      * MarkdownHelper constructor.
      *
-     * @param AdapterInterface  $adapterInterface
+     * @param AdapterInterface $adapterInterface
      * @param MarkdownInterface $markdownInterface
-     * @param LoggerInterface   $logger
+     * @param LoggerInterface $logger
+     * @param bool $isDebug
      */
-    public function __construct(AdapterInterface $adapterInterface, MarkdownInterface $markdownInterface, LoggerInterface $logger)
+    public function __construct(AdapterInterface $adapterInterface, MarkdownInterface $markdownInterface, LoggerInterface $logger, bool $isDebug)
     {
         $this->adapterInterface = $adapterInterface;
         $this->markdownInterface = $markdownInterface;
         $this->logger = $logger;
+        $this->isDebug = $isDebug;
     }
 
     /**
@@ -47,13 +53,20 @@ class MarkdownHelper
      */
     public function parse(string $source): string
     {
+
+        if ($this->isDebug) {
+            return $this->markdownInterface->transform($source);
+        }
+
         $item = $this->adapterInterface->getItem('markdown_'.md5($source));
+
 
         if (!$item->isHit()) {
             $item->set($this->markdownInterface->transform($source));
             $this->adapterInterface->save($item);
 
-            $this->logger->info('Cached Again');
+            $this->logger->alert('Cached Again');
+            $this->logger->info('Cached Again and Again');
         }
 
         return $item->get();
