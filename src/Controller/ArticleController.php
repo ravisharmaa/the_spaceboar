de6b29fefce2b6c8514d901f200ca672f864047b
ravi <ravi.sharma@javra.com>
2019-07-11 10:51:36 +0545

    Slack bundle and autowiring basics
    	new file:   config/packages/httplug.yaml

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,12 +3,30 @@
 namespace App\Controller;
 
 use App\Services\MarkdownHelper;
+use Nexy\Slack\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
+
+    /**
+     * @var bool
+     */
+    private $isDebug;
+    /**
+     * @var Client
+     */
+    private $slackClient;
+
+    public function __construct(bool $isDebug, Client $slackClient)
+    {
+
+        $this->isDebug = $isDebug;
+        $this->slackClient = $slackClient;
+    }
+
     /**
      * @Route ("/", name="homepage")
      *
@@ -25,11 +43,15 @@ class ArticleController extends AbstractController
      * @param $slug
      * @param MarkdownHelper $markdownHelper
      *
+     * @param bool $isDebug
+     * @param Client $slackClient
      * @return Response
      * @throws \Psr\Cache\InvalidArgumentException
      */
     public function show($slug, MarkdownHelper $markdownHelper)
     {
+        //dd($this->slackClient->send('hello world'));
+
         $comments = [
             'some random dymmy text',
         ];
